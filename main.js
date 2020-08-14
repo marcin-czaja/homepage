e53ae74ae2330167718e2c0814f5c916b5d46b87
Marcin Czaja <marcinczaja@outlook.com>
2020-08-15 00:21:58 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Siema!');
+console.log('witam');
+
+const Name='Marcin';
+const Age='21';
+
+console.log(Name);
+console.log(Age);
+
+console.log(`Hej, nazywam się ${Name} i mam ${Age} lat.`)
